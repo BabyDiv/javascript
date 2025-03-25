3cef6749651b01e5710296df9835b3c10df29b09
BabyDiv <inna.statsenko.work@gmail.com>
2025-03-25 14:23:49 +0500

    add some changes to js

---
+++ index.js
@@ -0,0 +1,17 @@
+// debugger;
+
+// let number;
+
+// number = null;
+
+// console.log(number);
+
+let array = [7, 6, 4, 5, 9];
+
+array.sort((a,b) => a - b);
+console.log(array);
+
+sentence = ['hello', 'world'];
+words = sentence.join();
+console.log(words);
+
