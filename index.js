4b65b2398c524708b01489dcbb5c20f740e2a0c0
BabyDiv <inna.statsenko.work@gmail.com>
2025-03-25 15:35:28 +0500

    add some changes to js

--- index.js
+++ index.js
@@ -15,3 +15,37 @@ sentence = ['hello', 'world'];
 words = sentence.join();
 console.log(words);
 
+let circle = document.querySelectorAll('.circles__item');
+let square = document.getElementsByClassName('squares__item');
+let circles = document.querySelector('.circles');
+let squares = document.querySelector('.squares');
+let triangle = document.getElementById('triangle');
+let div = document.createElement('div');
+let paragraph = document.createElement('p');
+
+circle[0].style.backgroundColor = 'purple';
+square[1].style.borderRadius = '50%';
+triangle.style.backgroundColor = 'lightgreen';
+
+for (let i = 0; i < square.length; i++) {
+  square[i].style.borderRadius = '50%';
+}
+
+circle.forEach(function(item, i) {
+  item.style.backgroundColor = 'pink';
+});
+
+div.classList.add('circles__item');
+console.log(div);
+squares.appendChild(div);
+
+paragraph.classList.add('circles__item');
+console.log(paragraph);
+circles.insertBefore(paragraph, circle[0]);
+paragraph.style.margin = '0';
+
+squares.removeChild(square[3]);
+circles.replaceChild(square[0], circle[0]);
+
+// paragraph.innerHTML = '<a>Ы</a>';
+// paragraph.textContent = 'ЫЫЫЫЫЫЫ';
